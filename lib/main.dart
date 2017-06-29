576a60d20db0bf32be5736fe46ff2ef6d53ee449
Devon Carew <devoncarew@google.com>
2017-06-29 12:49:36 -0700

    remove a print

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,6 @@ class SunflowerState extends State<SunflowerDemo> {
             activeColor: Theme.of(context).canvasColor,
             onChanged: (double value) {
               setState(() {
-                print('value: ${value.toStringAsFixed(2)}');
                 _value = value;
               });
             },
